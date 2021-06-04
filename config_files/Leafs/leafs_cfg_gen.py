7ec3b8191b7f5228045cef2292a53a4677ae66cc
sergeyn <sergeyn@playtika.com>
2021-06-05 00:02:40 +0300

    files reorganisation

--- config_files/Leafs/leafs_cfg_gen.py
+++ config_files/Leafs/leafs_cfg_gen.py
@@ -9,11 +9,26 @@ import yaml
 dir_path = "/Users/sergeyn/Data_Files/Online_Cources/OtusNetDesign/config_files/Leafs/"
 
 env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True, lstrip_blocks=True)
-template = env.get_template('leafs_underlay_ospf_cfg.j2')
+template_ospf = env.get_template('leafs_underlay_ospf_cfg.j2')
+template_isis = env.get_template('leafs_underlay_isis_cfg.j2')
 
-with open("data_files/leafs_params.yaml", 'r') as fr:
-    leafs = yaml.safe_load(fr)
-    for leaf in leafs:
-        filename = leaf['name']+'.ios'
-        with open(f"LEAFS/{filename}",'w') as fw:
-            fw.write(template.render(leaf))
+def ospf_config_gen(template):
+    with open("data_files/leafs_params.yaml", 'r') as fr:
+        leafs = yaml.safe_load(fr)
+        for leaf in leafs:
+            filename = leaf['name']+'_ospf.ios'
+            print(f'Config file {filename} has been created')
+            with open(f"LEAFS/OSPF_UNDERLAY/{filename}",'w') as fw:
+                fw.write(template.render(leaf))
+
+def isis_config_gen(template):
+    with open("data_files/leafs_params.yaml", 'r') as fr:
+        leafs = yaml.safe_load(fr)
+        for leaf in leafs:
+            filename = leaf['name']+'_isis.ios'
+            print(f'Config file {filename} has been created')
+            with open(f"LEAFS/ISIS_UNDERLAY/{filename}",'w') as fw:
+                fw.write(template.render(leaf))
+
+if __name__ == '__main__':
+    ospf_config_gen(template_ospf)
--- config_files/Spines/spines_cfg_gen.py
+++ config_files/Spines/spines_cfg_gen.py
@@ -6,15 +6,23 @@
 from jinja2 import Environment, FileSystemLoader
 import yaml
 
-dir_path = "/Users/sergeyn/Data_Files/Online_Cources/OtusNetDesign/config_files/Leafs/"
+dir_path = "/Users/sergeyn/Data_Files/Online_Cources/OtusNetDesign/config_files/Spines/"
 
 env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True, lstrip_blocks=True)
-template = env.get_template('spines_underlay_ospf_cfg.j2')
+template_ospf = env.get_template('spines_underlay_ospf_cfg.j2')
+template_isis = env.get_template('spines_underlay_isis_cfg.j2')
 
-with open("data_files/spines_params.yaml", 'r') as f:
-    spines = yaml.safe_load(f)
-    for spine in spines:
-        filename = spine['name']+'.ios'
-        print(filename)
-        with open(f"SPINES/{filename}",'w') as f:
-            f.write(template.render(spine))
+
+def ospf_config_gen(template):
+    with open("data_files/spines_params.yaml", 'r') as f:
+        spines = yaml.safe_load(f)
+        for spine in spines:
+            filename = spine['name']+'_ospf.ios'
+            print(f'Config file {filename} has been created')
+            with open(f"SPINES/OSPF_UNDERLAY/{filename}",'w') as f:
+                f.write(template.render(spine))
+
+
+
+if __name__ == '__main__':
+    ospf_config_gen(template_ospf)
