2b155d22f9f355dc379e2aedbbc3eb2b14c26aa5
sergeyn <sergeyn@playtika.com>
2021-06-27 17:18:35 +0300

    LEAFS_BGP dir and fix j2 template bgp underlay

--- config_files/Leafs/leafs_cfg_gen.py
+++ config_files/Leafs/leafs_cfg_gen.py
@@ -11,6 +11,7 @@ dir_path = "/Users/sergeyn/Data_Files/Online_Cources/OtusNetDesign/config_files/
 env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True, lstrip_blocks=True)
 template_ospf = env.get_template('leafs_underlay_ospf_cfg.j2')
 template_isis = env.get_template('leafs_underlay_isis_cfg.j2')
+template_ebgp = env.get_template('leafs_underlay_ebgp_cfg.j2')
 
 def ospf_config_gen(template):
     with open("data_files/leafs_params.yaml", 'r') as fr:
@@ -30,7 +31,16 @@ def isis_config_gen(template):
             with open(f"LEAFS/ISIS_UNDERLAY/{filename}",'w') as fw:
                 fw.write(template.render(leaf))
 
+def ebgp_config_gen(template):
+    with open("data_files/leafs_params.yaml", 'r') as fr:
+        leafs = yaml.safe_load(fr)
+        for leaf in leafs:
+            filename = leaf['name']+'_ebgp.ios'
+            print(f'Config file {filename} has been created')
+            with open(f"LEAFS/EBGP_UNDERLAY/{filename}",'w') as fw:
+                fw.write(template.render(leaf))
 
 if __name__ == '__main__':
     #ospf_config_gen(template_ospf)
-    isis_config_gen(template_isis)
+    #isis_config_gen(template_isis)
+    ebgp_config_gen(template_ebgp)
