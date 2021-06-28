b90bb420b5e5c5a217196c4336bc106d82e6dd31
sergeyn <sergeyn@playtika.com>
2021-06-28 10:20:39 +0300

    edditing config  file and templates

--- config_files/Spines/spines_cfg_gen.py
+++ config_files/Spines/spines_cfg_gen.py
@@ -11,7 +11,7 @@ dir_path = "/Users/sergeyn/Data_Files/Online_Cources/OtusNetDesign/config_files/
 env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True, lstrip_blocks=True)
 template_ospf = env.get_template('spines_underlay_ospf_cfg.j2')
 template_isis = env.get_template('spines_underlay_isis_cfg.j2')
-
+template_ebgp = env.get_template('spines_underlay_ebgp_cfg.j2')
 
 def ospf_config_gen(template):
     with open("data_files/spines_params.yaml", 'r') as f:
@@ -31,7 +31,16 @@ def isis_config_gen(template):
             with open(f"SPINES/ISIS_UNDERLAY/{filename}",'w') as fw:
                 fw.write(template.render(leaf))
 
+def ebgp_config_gen(template):
+    with open("data_files/spines_params.yaml", 'r') as fr:
+        leafs = yaml.safe_load(fr)
+        for leaf in leafs:
+            filename = leaf['name']+'_ebgp.ios'
+            print(f'Config file {filename} has been created')
+            with open(f"SPINES/EBGP_UNDERLAY/{filename}",'w') as fw:
+                fw.write(template.render(leaf))
 
 if __name__ == '__main__':
     #ospf_config_gen(template_ospf)
-    isis_config_gen(template_isis)
+    #isis_config_gen(template_isis)
+    ebgp_config_gen(template_ebgp)
